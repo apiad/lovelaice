79e9aeb5a168016bc886688c0b30ade8856dfc9a
Alejandro Piad-Morffis <apiad@apiad.net>
2023-10-03 15:18:37 -0400

    Add help

--- bot.py
+++ bot.py
@@ -21,15 +21,18 @@ api = MonsterAPI(api_key=os.getenv("MONSTER_API"))
 
 data_folder = Path(__file__).parent / "data" / "bot"
 
+with open(Path(__file__).parent / "bot_help.md") as fp:
+    help_text = fp.read()
 
-def get_data(user_id) -> Path:
+
+def _get_data(user_id) -> Path:
     user_folder = data_folder / str(user_id)
     user_folder.mkdir(parents=True, exist_ok=True)
     return user_folder
 
 
-def select_note(user_id, name=None):
-    selected_file = get_data(user_id) / ".selected-note"
+def _select_note(user_id, name=None):
+    selected_file = _get_data(user_id) / ".selected-note"
 
     if name:
         with open(selected_file, "w") as fp:
@@ -38,13 +41,13 @@ def select_note(user_id, name=None):
         selected_file.unlink(missing_ok=True)
 
 
-def get_selected_note(user_id) -> Path:
-    selected_file = get_data(user_id) / ".selected-note"
+def _get_selected_note(user_id) -> Path:
+    selected_file = _get_data(user_id) / ".selected-note"
 
     if not selected_file.exists():
         return None
 
-    return get_data(user_id) / str(open(selected_file).read())
+    return _get_data(user_id) / str(open(selected_file).read())
 
 
 logging.basicConfig(
@@ -91,7 +94,7 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = api.resolve(response)
     doc = Document(result["result"]["text"])
 
-    selected_note = get_selected_note(update.effective_chat.id)
+    selected_note = _get_selected_note(update.effective_chat.id)
 
     if selected_note:
         transcript_path = selected_note
@@ -101,7 +104,7 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
         title = "".join([c for c in title if c.isalnum() or c == " "])
         now = datetime.datetime.now().isoformat()
 
-        transcript_path = get_data(update.effective_chat.id) / f"{title} - {now}.txt"
+        transcript_path = _get_data(update.effective_chat.id) / f"{title} - {now}.txt"
 
     with transcript_path.open("a") as fp:
         for line in doc.sentences:
@@ -111,6 +114,8 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
         fp.write("\n")
 
     summary = str(doc)[:255] + "..."
+
+    _select_note(update.effective_chat.id, transcript_path.name)
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -124,7 +129,7 @@ _{summary}_
 
 
 async def msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    selected_note = get_selected_note(update.effective_chat.id)
+    selected_note = _get_selected_note(update.effective_chat.id)
 
     if not selected_note:
         await context.bot.send_message(
@@ -141,7 +146,7 @@ Send an audio or voice message to begin a new one."""
 
 
 async def txt(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    selected_note = get_selected_note(update.effective_chat.id)
+    selected_note = _get_selected_note(update.effective_chat.id)
 
     if not selected_note:
         await context.bot.send_message(
@@ -158,7 +163,7 @@ Send an audio or voice message to begin a new one."""
 
 
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    selected_note = get_selected_note(update.effective_chat.id)
+    selected_note = _get_selected_note(update.effective_chat.id)
 
     if not selected_note:
         await context.bot.send_message(
@@ -168,7 +173,7 @@ Send an audio or voice message to begin a new one."""
         )
         return
 
-    select_note(update.effective_chat.id)
+    _select_note(update.effective_chat.id)
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text="Done. Send me an audio message to start a new note."
@@ -176,7 +181,7 @@ Send an audio or voice message to begin a new one."""
 
 
 async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    selected_note = get_selected_note(update.effective_chat.id)
+    selected_note = _get_selected_note(update.effective_chat.id)
 
     if not selected_note:
         await context.bot.send_message(
@@ -187,7 +192,7 @@ Send an audio or voice message to begin a new one."""
         return
 
     selected_note.unlink()
-    select_note(update.effective_chat.id)
+    _select_note(update.effective_chat.id)
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text="Note discarded. Send me an audio message to start a new note."
@@ -195,7 +200,7 @@ Send an audio or voice message to begin a new one."""
 
 
 async def select(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    notes = [path.name for path in get_data(update.effective_chat.id).glob("*.txt")]
+    notes = [path.name for path in _get_data(update.effective_chat.id).glob("*.txt")]
     mapping = {f"/note_{i+1}": note for i,note in enumerate(notes)}
 
     note = update.effective_message.text
@@ -206,10 +211,10 @@ async def select(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
 
-    select_note(update.effective_chat.id, mapping[note])
+    _select_note(update.effective_chat.id, mapping[note])
 
     await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f"""Selected note: **{note}**
+            chat_id=update.effective_chat.id, text=f"""Selected note: **{mapping[note]}**
 
 Send a voice message to append to this note, or use the following commands:
 
@@ -217,11 +222,11 @@ Send a voice message to append to this note, or use the following commands:
 /txt - Download note as TXT file.
 /delete - Delete this note (undoable!)
 /done - Finish with this note.
-"""
+""", parse_mode="markdown"
         )
 
 async def list(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    notes = [path.name for path in get_data(update.effective_chat.id).glob("*.txt")]
+    notes = [path.name for path in _get_data(update.effective_chat.id).glob("*.txt")]
     msg = "\n".join(f"/note_{i+1} - {note}" for i, note in enumerate(notes))
 
     if not msg:
@@ -231,6 +236,12 @@ async def list(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id, text=msg
     )
 
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=help_text, parse_mode="markdown"
+    )
 
 async def default(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -259,6 +270,9 @@ if __name__ == "__main__":
 
     delete_handler = CommandHandler("delete", delete)
     application.add_handler(delete_handler)
+
+    help_handler = CommandHandler("help", help)
+    application.add_handler(help_handler)
 
     audio_handler = MessageHandler(filters.VOICE, transcribe)
     application.add_handler(audio_handler)
---
+++ bot_help.md
@@ -0,0 +1,9 @@
+I can transcribe audio messages for you and store them as text notes. You can send one or multiple audios to a single note.
+
+Start by sending an audio message, which will create a new note. After the transcription is finished, you can send more audios to continue the same note, or create new notes.
+
+Send /list to see all stored notes.
+
+In your message, you can use voice commands to do some formatting. For example, try saying "title, here is a title" or "heading, this is a heading" and the corresponding text will be formatted as title or heading in markdown (using # or ##).
+
+You can also say "begin list", "end list" and the items you say in between will be formatted as a list. Also works with "(begin / end) ordered list" and "(begin / end) to-do list".
