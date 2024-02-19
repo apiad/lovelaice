29002be5edb42d5a456b7024c45f16588aaa0b2f
Alejandro Piad-Morffis <apiad@apiad.net>
2024-02-19 06:33:32 -0500

    Add proper script configuration

--- lovelaice/__main__.py
+++ lovelaice/__main__.py
@@ -1,26 +1,4 @@
-import os
-import time
-import dotenv
-import argparse
-from mistralai.client import MistralClient
-from .core import query
-
-
-dotenv.load_dotenv()
-
-parser = argparse.ArgumentParser("lovelaice")
-parser.add_argument("-f", "--file", action="store", help="Add a file to the context")
-parser.add_argument("--model", action="store", help="Select model", default="mistral-small")
-parser.add_argument("query", nargs="+")
-
+from .cli import run
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    client = MistralClient(api_key=os.getenv("MISTRAL_API_KEY"))
-
-    prompt = " ".join(args.query)
-
-    for response in query(prompt, client, args.model):
-        print(response, end="", flush=True)
-
-    print()
+    run()
